1daf47974cc381cfef7bb4b6dfc3030abfab1614
kumankoishi.py <7iggif@naver.com>
2020-07-17 14:17:18 +0900

    Implementing normal shots

--- Assets/Scripts/PlayerNormalAttack.cs
+++ Assets/Scripts/PlayerNormalAttack.cs
@@ -7,6 +7,8 @@ public class PlayerNormalAttack : MonoBehaviour
     [Header("Shot Specification")]
     [Tooltip("Maximum level of normal shots")]
     public int maxLevel = 4;
+    public float roundsPerSecond = 10f;
+    public GameObject projectilePrefab;
 
     public int curLevel { get; private set; }
 
@@ -14,19 +16,31 @@ public class PlayerNormalAttack : MonoBehaviour
     private PlayerDirection direction;
     private CharacterController controller;
 
+    private Vector3[] shotDirections = { };
+    private int shotSeq;
+    private float secondsPerRound;
+    private float fireTimer;
+
     // Start is called before the first frame update
     void Start()
     {
         aimPoint = transform.Find("NormalAimPoint").gameObject;
         direction = GetComponent<PlayerDirection>();
         controller = GetComponent<CharacterController>();
+
         curLevel = 1;
+        shotSeq = 0;
+
+        secondsPerRound = 1 / roundsPerSecond;
+        fireTimer = secondsPerRound;
     }
 
     // Update is called once per frame
     void Update()
     {
         SetPointDirection();
+        Fire();
+        ProgressTimer();
     }
 
     void SetPointDirection()
@@ -54,6 +68,33 @@ public class PlayerNormalAttack : MonoBehaviour
             }
         }
 
-        aimPoint.transform.localPosition = new Vector3(finalXDirection, finalYDirection, 0).normalized;
+        aimPoint.transform.localPosition = new Vector3(finalXDirection, finalYDirection, 0);
+    }
+
+    void Fire()
+    {
+        if (isFireable() && Input.GetButton("Fire1"))
+        {
+            Vector3 shootDirection = aimPoint.transform.position - transform.position;
+            GameObject projectile = Instantiate(projectilePrefab, aimPoint.transform);
+            PlayerProjectile pp = projectile.transform.gameObject.GetComponent<PlayerProjectile>();
+            pp.direction = shootDirection.normalized;
+
+            projectile.transform.SetParent(null);
+            fireTimer = 0f;
+        }
+    }
+
+    void ProgressTimer()
+    {
+        if (fireTimer < secondsPerRound)
+        {
+            fireTimer += Time.deltaTime;
+        }
+    }
+
+    bool isFireable()
+    {
+        return (fireTimer >= secondsPerRound);
     }
 }
---
+++ Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    [Range(1, 2)]
+    public int shooter = 1;
+    public float speed = 20f;
+    public Vector3 direction;
+    public float lifetime = 2f;
+
+    private float timeSpan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSpan = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GoForward();
+        Timer();
+    }
+
+    void GoForward()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Collision method here
+
+        Destroy(gameObject);
+    }
+
+    private void Timer()
+    {
+        timeSpan += Time.deltaTime;
+        if (timeSpan >= lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
