e66e84be7e5f383f5ac4b67b64a8be83137ef2c2
kumankoishi.py <7iggif@naver.com>
2020-07-20 13:37:58 +0900

    Implementing sample enemy generation

--- Assets/Scripts/SampleEnemyGenerator.cs
+++ Assets/Scripts/SampleEnemyGenerator.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SampleEnemyGenerator : MonoBehaviour
 {
+    public float secondsPerEnemy = 1f;
+    public GameObject testEnemyPrefab;
+    public Transform startPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(GenerateSampleEnemy());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator GenerateSampleEnemy()
+    {
+        while(true)
+        {
+            GameObject flyingEnemy = Instantiate(testEnemyPrefab, startPoint);
+            flyingEnemy.transform.SetParent(null);
+            yield return new WaitForSeconds(secondsPerEnemy);
+        }
     }
 }
