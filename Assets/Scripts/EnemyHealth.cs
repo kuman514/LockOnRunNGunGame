0f6ead0798802a9a61c65b22ca13ce17c9a21882
kuman514 <7iggif@naver.com>
2020-07-23 15:08:44 +0900

    Implemented Enemy Health, Enemy Hit and get Damaged by Player's Projectiles

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float HP;
+
+    public float curHP { get; private set; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHP = HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckState();
+    }
+
+    private void CheckState()
+    {
+        if (curHP <= 0)
+        {
+            // destroy method
+
+            Destroy(gameObject);
+        }
+    }
+
+    public void Damage(float dmg)
+    {
+        curHP -= dmg;
+    }
+}
--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
 {
+    [Header("Player projectile info")]
+    [Tooltip("Who fired this shot? Player 1, or Player 2?")]
     [Range(1, 2)]
     public int shooter = 1;
+    [Tooltip("Speed of this projectile")]
     public float speed = 20f;
+    [Tooltip("Projectile forwarding direction")]
     public Vector3 direction;
+    [Tooltip("Duration of this projectile")]
     public float lifetime = 2f;
+    [Tooltip("Strength of this projectile")]
+    public float damage = 10f;
 
+    // inner active
     private float timeSpan;
+    public float dmg { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
         timeSpan = 0f;
+        dmg = damage;
     }
 
     // Update is called once per frame
@@ -33,15 +44,7 @@ public class PlayerProjectile : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         // Collision method here
-        Debug.Log("Projectile Collided");
-
-        Destroy(gameObject);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // Collision method here
-        Debug.Log("Projectile Collided");
+        CollideWithEnemy(collision);
 
         Destroy(gameObject);
     }
@@ -52,6 +55,19 @@ public class PlayerProjectile : MonoBehaviour
         if (timeSpan >= lifetime)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void CollideWithEnemy(Collision col)
+    {
+        if (col.transform.CompareTag("Target"))
+        {
+            UnityEngine.Debug.Log("hit enemy");
+            EnemyHealth eh = col.transform.GetComponent<EnemyHealth>();
+            if (eh != null)
+            {
+                eh.Damage(dmg);
+            }
         }
     }
 }
