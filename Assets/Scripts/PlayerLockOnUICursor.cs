0c879f37ccbccb22d0a639e58eda61ab4c7fa370
kuman514 <7iggif@naver.com>
2020-07-27 22:11:42 +0900

    Lock-on cursor now on the UI

---
+++ Assets/Scripts/PlayerLockOnUICursor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerLockOnUICursor : MonoBehaviour
+{
+    public GameObject player;
+
+    private PlayerLockOnAttack pla;
+    private RectTransform rt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pla = player.transform.GetComponent<PlayerLockOnAttack>();
+        rt = transform.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetUIPosition();
+    }
+
+    void SetUIPosition()
+    {
+        rt.position = pla.lockonCursorPos;
+    }
+}
