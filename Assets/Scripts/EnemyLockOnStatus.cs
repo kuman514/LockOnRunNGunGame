bab66e8b9f8d57f0a373566313c1924397452e7c
kuman514 <7iggif@naver.com>
2020-07-30 00:43:58 +0900

    Adding Enemy Lockon Status

--- Assets/Scripts/EnemyLockOnStatus.cs
+++ Assets/Scripts/EnemyLockOnStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
 
@@ -15,18 +16,34 @@ public class EnemyLockOnStatus : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        isLockedOn = false;
+        lockonTiming = 0f;
         cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        PaintLockOn();
         IncreaseTiming();
+    }
+
+    public void SetLockedOn()
+    {
+        isLockedOn = true;
+    }
+
+    void PaintLockOn()
+    {
+        if (isLockedOn)
+        {
+            Vector3 uiPos = cam.WorldToScreenPoint(transform.position);
+        }
     }
 
     bool CheckLockable()
     {
-        return (lockOnTiming >= secondsAfterLockedOn);
+        return (lockonTiming >= secondsAfterLockedOn);
     }
 
     void IncreaseTiming()
