e0d87024525720b59e175e05ebb58cff3af98b67
kumankoishi.py <7iggif@naver.com>
2020-08-20 00:00:35 +0900

    Implementing Continue when all players are inactive

--- Assets/Scripts/PlayerLifeSystem.cs
+++ Assets/Scripts/PlayerLifeSystem.cs
@@ -18,6 +18,7 @@ public class PlayerLifeSystem : MonoBehaviour
 
     private float P1ResTiming;
     private float P2ResTiming;
+    private UIContinue cont;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +28,8 @@ public class PlayerLifeSystem : MonoBehaviour
 
         P1ResTiming = respawnInterval;
         P2ResTiming = respawnInterval;
+
+        cont = GetComponent<UIContinue>();
     }
 
     // Update is called once per frame
@@ -35,6 +38,7 @@ public class PlayerLifeSystem : MonoBehaviour
         GetPressStart();
         CheckPlayerIsDead();
         IncreaseTiming();
+        SetGameOver();
     }
 
     void GetPressStart()
@@ -45,6 +49,7 @@ public class PlayerLifeSystem : MonoBehaviour
             P1Left = totalLife - 1;
             P1ResTiming = 0f;
             P1.SetActive(true);
+            cont.enabled = false;
         }
 
         if (GameInput.GetStart(1))
@@ -53,6 +58,7 @@ public class PlayerLifeSystem : MonoBehaviour
             P2Left = totalLife - 1;
             P2ResTiming = 0f;
             P2.SetActive(true);
+            cont.enabled = false;
         }
     }
 
@@ -97,6 +103,14 @@ public class PlayerLifeSystem : MonoBehaviour
         if (P2.activeSelf == false && P2ResTiming < respawnInterval)
         {
             P2ResTiming += Time.deltaTime;
+        }
+    }
+
+    void SetGameOver()
+    {
+        if (P1Left < 0 && P2Left < 0 && !P1.activeSelf && !P2.activeSelf)
+        {
+            cont.enabled = true;
         }
     }
 }
---
+++ Assets/Scripts/UIContinue.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIContinue : MonoBehaviour
+{
+    public int continueCount = 20;
+
+    public int count { get; private set; }
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+        Beep();
+        Countdown();
+    }
+
+    void Timer()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void Beep()
+    {
+        if (timer >= 1f)
+        {
+
+        }
+    }
+
+    void Countdown()
+    {
+        if (timer >= 1f)
+        {
+            timer = 0f;
+            count--;
+        }
+    }
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0f;
+        count = continueCount;
+        timer = 1f;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
+        timer = 0f;
+    }
+}
