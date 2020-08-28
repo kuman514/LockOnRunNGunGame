051697cfbdba948d2abc83730827e93f6fbcbc05
kumankoishi.py <7iggif@naver.com>
2020-08-28 17:41:42 +0900

    Making Enemy Behaviors

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyBehavior
+{
+    public enum BehaviorCode { MoveAndRotate, Shoot, LookPlayer, Destroy, Loop }
+
+    public BehaviorCode behaviorCode;
+}
